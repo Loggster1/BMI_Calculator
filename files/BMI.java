b4b852a17e2bae83da6390995451afc07cc86baf
Loggster1 <152931273+Loggster1@users.noreply.github.com>
2025-04-04 00:34:03 -0500

    Update BMI.java

--- files/BMI.java
+++ files/BMI.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-
 /**
- *
- * @author Loggster
+ * Logan Wallarab
+ * www.loggster.dev
  */
 public class BMI{
 
